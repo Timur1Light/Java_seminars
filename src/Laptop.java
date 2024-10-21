7627697c1e9ffc6e5d277564cf708ea6d5c6cc61
Timur.Light <Timur.Light@mail.ru>
2024-10-21 21:55:28 +0900

    add class Laptop.java

---
+++ src/Laptop.java
@@ -0,0 +1,57 @@
+import java.util.Objects;
+
+
+public class Laptop {
+    //Описываем поля
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+
+    //Создаем конструктор
+    public Laptop(int ram, int hdd, String os, String color) {
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    //Поведение обьекта
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop -> <<" +
+                "RAM=" + ram + " GB" +
+                ", HDD=" + hdd + " GB" +
+                ", OS='" + os +
+                ", Color='" + color + ">>";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Laptop laptop = (Laptop) o;
+        return ram == laptop.ram && hdd == laptop.hdd && os.equals(laptop.os) && color.equals(laptop.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ram, hdd, os, color);
+    }
+}
